af115210806f393b04e582c1efbfc75af36deb2f
David Griffiths <reesdaviesgriffiths@hotmail.com>
2021-10-20 20:26:32 +0100

    Add project files.

---
+++ TestProject2/UnitTest1.cs
@@ -0,0 +1,57 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace TestProject2
+{
+    public class SearchingGoogle
+    {
+        string test_url = "https://google.co.uk";
+
+        IWebDriver driver;        
+        
+        [SetUp]
+        public void start_Browser()
+        {
+            //local Selenium WebDriver
+            driver = new ChromeDriver(@"C:\Program Files\Google\Chrome\Application");
+            driver.Manage().Window.Maximize();
+
+
+        }
+
+        [Test]
+        public void test_search()
+        {
+            driver.Url = test_url;
+
+            System.Threading.Thread.Sleep(2000);
+
+            //if ID is visible
+            //{click on the button}
+            
+            IWebElement isTAndCsPagePresent = driver.FindElement(By.Id("L2AGLb"));
+
+            isTAndCsPagePresent.Click;
+
+            //else {do the below code or IF NOT visible do the same code}
+            IWebElement searchText = driver.FindElement(By.CssSelector("[name = 'q']"));
+
+            searchText.SendKeys("LambdaTest");
+            searchText.SendKeys(Keys.Enter);
+
+            System.Threading.Thread.Sleep(2000);
+
+            Console.WriteLine("the Test passed");
+
+            }
+
+        [TearDown]
+
+        public void close_Browser()
+        {
+            driver.Quit();
+        }
+    }
+}
